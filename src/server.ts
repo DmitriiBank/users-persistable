66495ce43fd32c7edd2f370914d85c1c0a740bb9
DmitriiBank <dm.skakov@gmail.com>
2025-07-27 19:14:10 +0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,31 @@
 import {createServer} from "node:http";
 import {PORT} from "./config/userServerConfig.ts";
-import {userRouters} from "./routers/userRoutes.ts";
+import {userRouters} from "./routers/userRouters.ts";
 import {UserController} from "./controllers/UserController.ts";
 import {UserServiceEmbeddedImpl} from "./service/UserServiceEmbeddedImpl.ts";
-import {myLogger} from "./utils/logger.ts";
+import {myLogger} from "./events/logger.ts";
 
 export const launchServer = () => {
     const userService = new UserServiceEmbeddedImpl();
-    userService.restoreDataFromFile();
-    const userController:UserController = new UserController(userService);
+    userService.restoreDataFromFile()
+    const userController:UserController = new UserController(userService)
 
     createServer(async (req, res) => {
-      await userRouters(req, res, userController) ;
+        await userRouters(req, res, userController)
     }).listen(PORT, () => {
         console.log(`UserServer runs at http://localhost:${PORT}`)
     })
 
-    process.on('SIGINT',async () => {
-        await userService.saveDataToFile();
-        myLogger.log("Saving....")
-        myLogger.saveToFile("Server shutdown by Ctrl+C");
-        process.exit();
+    process.on('SIGINT', async () => {
+        // userService.saveDataToFileSync()
+        try {
+            await userService.saveDataToFile();
+            myLogger.log("Saving...")
+            myLogger.saveToFile('Server shutdown by Ctrl + C')
+            process.exit(0)
+        }catch (error) {
+            console.error('Error during shutdown:', error);
+            process.exit(1);
+        }
     })
 }
--- src/service/UserServiceEmbeddedImpl.ts
+++ src/service/UserServiceEmbeddedImpl.ts
@@ -1,9 +1,14 @@
 import {UserService} from "./UserService.js";
 import {User} from "../model/userTypes.js";
+import {UserFilePersistenceService} from "./UserFilePersistenceService.js";
+import fs from "fs";
+import {myLogger} from "../events/logger.js";
 
 
-export class UserServiceEmbeddedImpl implements UserService{
+export class UserServiceEmbeddedImpl implements UserService, UserFilePersistenceService{
     private users: User[] = [{id:1, userName:"Bond"}]
+    private rs = fs.createReadStream('data.txt', {encoding: "utf-8", highWaterMark: 24})
+
     addUser(user: User): boolean {
         if(this.users.findIndex((u:User) => u.id === user.id) === -1) {
             this.users.push(user)
@@ -36,6 +41,79 @@ export class UserServiceEmbeddedImpl implements UserService{
             return true
         }
         return false
+    }
+
+    restoreDataFromFile(): string {
+        let result = ""
+        this.rs.on('data', (chunk) => {
+            if(chunk)
+                result += chunk.toString()
+            else {
+                result = "[]"
+            }
+
+        })
+
+        this.rs.on('end', () => {
+            if(result){
+                this.users = JSON.parse(result)
+                myLogger.log("Data was restored from file")
+                myLogger.save("Data was restored from file")
+                this.rs.close()
+            }else {
+                this.users = [{id:3, userName:"Panikovsky"}]
+            }
+        })
+
+        this.rs.on('error', () => {
+            this.users = [{id:2, userName:"Bender"}]
+            myLogger.log("File to restored not found")
+        })
+        return "Ok";
+    }
+
+    saveDataToFile(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            try {
+                const ws = fs.createWriteStream('data.txt', {flags: "w"})
+                myLogger.log("Ws created")
+                const data = JSON.stringify(this.users);
+                myLogger.log(data)
+                ws.write((data), (e) => {
+                    if(e)
+                        myLogger.log("Error!" + e?.message)
+                })
+                ws.on('finish', () => {
+                    myLogger.log("Data was saved to file");
+                    myLogger.save("Data was saved to file");
+                    ws.end();
+                })
+                ws.on('error', () => {
+                    myLogger.log("error: data not saved!")
+                })
+                return "Ok";
+            } catch (error) {
+                console.error('saveDataToFile error:', error);
+                myLogger.log('saveDataToFile error: ');
+            }
+
+            return "Ok";
+        })
+    }
+
+    saveDataToFileSync(): string {
+        try {
+            const data = JSON.stringify(this.users);
+            fs.writeFileSync('data.txt', data, 'utf-8');
+            myLogger.log("Data was saved to file synchronously");
+            myLogger.save("Data was saved to file synchronously");
+
+            return "Ok";
+        } catch (error) {
+            console.error('Error saving data:', error);
+            myLogger.log('Error saving data');
+            return "Error";
+        }
     }
 };
 
